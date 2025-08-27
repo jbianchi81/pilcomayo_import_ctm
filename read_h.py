3e4a21f1af97e5f9e4beb903684978346c35128b
jbianchi81 <jbianchi@ina.gob.ar>
2025-08-27 11:59:22 -0300

    skip n lines

--- read_h.py
+++ read_h.py
@@ -1,14 +1,16 @@
 import pandas
 from a5client import client
+import matplotlib
 
 # importCSV("villamontes.csv", 42291)
 
-# filename = "misionlapaz_3.csv"
+# filename = "misionlapaz_4.csv"
 # series_id = 42292
-# value_column="Altura Horaria (m)"
+# value_column = "Altura Horaria (m)"
+# head_skip_lines = 5
 
-def importCSV(filename : str, series_id : int, round_hour_only=True, value_column="Altura Horaria (m)"):
-    data = pandas.read_csv(filename)
+def importCSV(filename : str, series_id : int, round_hour_only=True, value_column="Altura Horaria (m)", head_skip_lines=5, plot=False):
+    data = pandas.read_csv(filename, skiprows=head_skip_lines)
     data["Fecha"].fillna(method="ffill", inplace=True)
     data["timestart"] = pandas.to_datetime(data["Fecha"], format="%d-%m-%Y")
     data["timestart"] = data["timestart"] + pandas.to_timedelta(data["Hora"])
@@ -17,8 +19,12 @@ def importCSV(filename : str, series_id : int, round_hour_only=True, value_colum
     data["tipo"] = "puntual"
     data = data.set_index("timestart")
     if round_hour_only:
+        # deja sólo los valores de las horas en punto
         data = data[(data.index.minute == 0) & (data.index.second == 0)]
     data = data.rename(columns={value_column: "valor"})
+    if plot:
+        data.plot(y="valor")
+        matplotlib.pyplot.show()
     client.createObservaciones(data, series_id=series_id)
 
 # importCSV("misionlapaz_3.csv", 42292)
